b12ef130570827caf0e59bae1548aa7097d6a1c8
Renan <renan@freework.io>
2020-12-14 17:30:22 +0100

    Adds first version of translator

--- apps/translator/src/app/app.tsx
+++ apps/translator/src/app/app.tsx
@@ -1,7 +1,7 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { addedDiff } from 'deep-object-diff';
 import _ from 'lodash';
-
+import { motion } from 'framer-motion';
 import './app.scss';
 
 import { ReactComponent as Logo } from './logo.svg';
@@ -11,6 +11,12 @@ import { Route, Link } from 'react-router-dom';
 import { useDropzone } from 'react-dropzone';
 import { type } from 'os';
 import ReactJson from 'react-json-view';
+
+const buttonClass =
+  'py-2 px-4 bg-green-500 text-white font-semibold rounded-lg shadow-md focus:outline-none';
+
+const buttonClass2 =
+  'py-2 px-4 bg-pink-300 text-white font-semibold rounded-lg shadow-md focus:outline-none mr-2';
 
 function difference(object, base) {
   function changes(object, base) {
@@ -26,6 +32,32 @@ function difference(object, base) {
   return changes(object, base);
 }
 
+const TitleAndDescription = ({ title, description }) => {
+  return (
+    <>
+      <h1 className="max-w-4xl text-green-500 my-6 font-spoof text-4xl md:text-5xl text-black leading-snug md:leading-extra-loose mx-auto tracking-tight text-center font-bold">
+        {title}
+      </h1>
+      <p className="font-ttnorms text-gray-500 text-xl leading-relaxed text-center mb-3 mx-auto">
+        {description}
+      </p>
+    </>
+  );
+};
+
+const TitleAndDescription2 = ({ title, description }) => {
+  return (
+    <>
+      <h1 className="max-w-4xl text-pink-500 my-6 font-spoof text-4xl md:text-5xl text-black leading-snug md:leading-extra-loose mx-auto tracking-tight text-center font-bold">
+        {title}
+      </h1>
+      <p className="font-ttnorms text-gray-500 text-xl leading-relaxed text-center mb-3 mx-auto">
+        {description}
+      </p>
+    </>
+  );
+};
+
 const EntryJson = ({ onUploadComplete }) => {
   const onDrop = useCallback((files) => {
     const fr = new FileReader();
@@ -40,10 +72,22 @@ const EntryJson = ({ onUploadComplete }) => {
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
   return (
-    <div {...getRootProps()}>
-      <input {...getInputProps()} />
-      {isDragActive ? <p>Base JSON</p> : <p>Add here for example your zu</p>}
-    </div>
+    <>
+      <TitleAndDescription
+        title="Select your base language"
+        description="For instance, you zu.json"
+      />
+      <div {...getRootProps()}>
+        <input {...getInputProps()} />
+        {isDragActive ? (
+          <p>Base JSON</p>
+        ) : (
+          <div className="p-4 bg-white-300 text-black text-center font-extrabold flex items-center justify-center border-8 border-green-300">
+            <p>Drag here</p>
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
@@ -60,22 +104,48 @@ const OutputJson = ({ onUploadComplete }) => {
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
   return (
-    <div {...getRootProps()}>
-      <input {...getInputProps()} />
-      {isDragActive ? (
-        <p>Language Json</p>
-      ) : (
-        <p>Add here for example your de.json</p>
-      )}
-    </div>
+    <>
+      <TitleAndDescription2
+        title="Now, Select your base language"
+        description="For instance, you de.json"
+      />
+      <div {...getRootProps()}>
+        <input {...getInputProps()} />
+        {isDragActive ? (
+          <p>Language Json</p>
+        ) : (
+          <div className="p-4 bg-white-300 text-black text-center font-extrabold flex items-center justify-center border-8 border-pink-300">
+            <p>Drag here</p>
+          </div>
+        )}
+      </div>
+    </>
   );
 };
+
+export const Reveal: React.FC<{ isOpen: boolean }> = ({ children, isOpen }) => (
+  <motion.div
+    initial={{ opacity: 0 }}
+    transition={{ duration: 0.3, delay: 0 }}
+    animate={{
+      height: isOpen ? 'auto' : 0,
+      opacity: isOpen ? 1 : 0,
+      y: isOpen ? 0 : -40,
+      x: isOpen ? 0 : -40,
+      pointerEvents: !isOpen ? 'none' : 'initial',
+    }}
+  >
+    <motion.div transition={{ duration: 0.9, delay: 1 }}></motion.div>
+    {children}
+  </motion.div>
+);
 
 export const App = () => {
   const useBaseJson = useState();
   const useLanguageJson = useState();
   const useResultJson = useState();
   const useFinishJson = useState();
+  const [step, goTo] = useState(1);
 
   const onDiff = () => {
     const newJson = addedDiff(useLanguageJson[0], useBaseJson[0]);
@@ -89,19 +159,127 @@ export const App = () => {
     useFinishJson[1](newJson);
   };
 
+  useEffect(() => {
+    onDiff();
+  }, [JSON.stringify(useBaseJson[0]), JSON.stringify(useLanguageJson[0])]);
+
+  // return (
+  //   <div className="container mx-auto">
+  //     <EntryJson onUploadComplete={useBaseJson[1]} />
+  //     <OutputJson onUploadComplete={useLanguageJson[1]} />
+  //     <button className={buttonClass} onClick={onDiff}>
+  //       merge
+  //     </button>
+  //     STRINGSTO
+  //     <ReactJson
+  //       src={useResultJson[0]}
+  //       onEdit={({ updated_src }) => useResultJson[1](updated_src)}
+  //     />
+  //     <button className={buttonClass} onClick={onFinish}>
+  //       merge
+  //     </button>
+  //     Your JSON
+  //     <ReactJson src={useFinishJson[0]} />
+  //   </div>
+  // );
+
+  const updateLanguageJsonAndMerge = (v) => {
+    useLanguageJson[1](v);
+    goTo(3);
+  };
+
   return (
     <>
-      <EntryJson onUploadComplete={useBaseJson[1]} />
-      <OutputJson onUploadComplete={useLanguageJson[1]} />
-      <button onClick={onDiff}>merge</button>
-      STRINGSTO
-      <ReactJson
-        src={useResultJson[0]}
-        onEdit={({ updated_src }) => useResultJson[1](updated_src)}
-      />
-      <button onClick={onFinish}>merge</button>
-      Your JSON
-      <ReactJson src={useFinishJson[0]} />
+      <Reveal isOpen={step === 1}>
+        <div
+          style={{ minWidth: '100%', maxWidth: '400', padding: 32 }}
+          className="container text-center mt-4 mx-400"
+        >
+          <div className="mt-4" />
+
+          <EntryJson
+            onUploadComplete={(v) => {
+              useBaseJson[1](v);
+              goTo(2);
+            }}
+          />
+        </div>
+      </Reveal>
+
+      <Reveal isOpen={step === 2}>
+        <div
+          style={{ minWidth: '100%', maxWidth: '400', padding: 32 }}
+          className="container text-center mt-4 mx-400"
+        >
+          <div className="mt-4" />
+
+          <OutputJson onUploadComplete={updateLanguageJsonAndMerge} />
+        </div>
+      </Reveal>
+
+      <Reveal isOpen={step === 3}>
+        <div
+          style={{ minWidth: '100%', maxWidth: '400', padding: 32 }}
+          className="container text-center mt-4 mx-400"
+        >
+          <div className="mt-4" />
+
+          <TitleAndDescription
+            title="This is the strings you need to update"
+            description="Update the translations then click generate output"
+          />
+
+          <ReactJson
+            src={useResultJson[0]}
+            onEdit={({ updated_src }) => useResultJson[1](updated_src)}
+          />
+
+          <a
+            href={`data:text/json;charset=utf-8,${encodeURIComponent(
+              JSON.stringify(useResultJson[0])
+            )}`}
+            download="diff.json"
+            className={buttonClass2}
+          >
+            Download output
+          </a>
+
+          <button
+            className={buttonClass}
+            onClick={() => {
+              onFinish();
+              goTo(4);
+            }}
+          >
+            Generate output
+          </button>
+        </div>
+      </Reveal>
+
+      <Reveal isOpen={step === 4}>
+        <div
+          style={{ minWidth: '100%', maxWidth: '400', padding: 32 }}
+          className="container text-center mt-4 mx-400"
+        >
+          <div className="mt-4" />
+
+          <TitleAndDescription
+            title="JSON DONE"
+            description="You can download it! "
+          />
+
+          <ReactJson src={useFinishJson[0]} />
+          <a
+            href={`data:text/json;charset=utf-8,${encodeURIComponent(
+              JSON.stringify(useFinishJson[0])
+            )}`}
+            download="output.json"
+            className={buttonClass}
+          >
+            Download it
+          </a>
+        </div>
+      </Reveal>
     </>
   );
 };
